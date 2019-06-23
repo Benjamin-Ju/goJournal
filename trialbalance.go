ca05d48246431338317cc0915963bb5b4fdb67ca
Benjamin Ju <benjaminyanju@gmail.com>
2019-06-22 22:20:20 -0400

    Added len check for both accounts for later file writing

--- trialbalance.go
+++ trialbalance.go
@@ -3,6 +3,8 @@ package main
 type trialBalance map[string]int
 
 func (t *trialBalance) createTrialBalance(j journal) {
+	maxLengthCredit := ""
+	maxLengthDebit := ""
 	tempTrialBalance := trialBalance{}
 	for _, entry := range j {
 		for index, debit := range entry.debitEntries {
@@ -11,12 +13,18 @@ func (t *trialBalance) createTrialBalance(j journal) {
 			} else {
 				tempTrialBalance[debit] = entry.debitBalances[index]
 			}
+			if len(debit) > len(maxLengthDebit) {
+				maxLengthDebit = debit
+			}
 		}
 		for index, credit := range entry.creditEntries {
 			if _, ok := tempTrialBalance[credit]; ok {
 				tempTrialBalance[credit] -= entry.creditBalances[index]
 			} else {
 				tempTrialBalance[credit] = -entry.creditBalances[index]
+			}
+			if len(credit) > len(maxLengthCredit) {
+				maxLengthCredit = credit
 			}
 		}
 
